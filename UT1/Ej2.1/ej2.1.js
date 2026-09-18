095673c4610cc85f4f523aed2d05f2a573d4b042
jose.ortgar <jose.ortgar@educa.jcyl.es>
2026-09-18 12:49:47 +0200

    inixio

---
+++ UT1/Ej2.1/ej2.1.js
@@ -0,0 +1,11 @@
+const MAX=100;
+let seguir= true;
+
+
+for (let i=0; seguir==true; i++) {
+    console.log(i);
+    if (i == MAX) seguir=false;
+
+}
+
+
---
+++ UT1/script1.2.js
@@ -0,0 +1,10 @@
+
+
+    for (let i = 1; i <= 100; i++) {
+        document.body.innerHTML += "<p>" + i + "</p>";
+    }
+
+    for (let i = 900; i <= 1000; i++) {
+        document.body.innerHTML += "<p>" + i + "</p>";
+    }
+
